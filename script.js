3978ae1341c2b4377eaf94454a5d96184ac90762
MohammadAlAgha <mohalagha11@gmail.com>
2023-02-25 17:23:50 +0200

    Added the pass and the emails and saved in JSON

--- script.js
+++ script.js
@@ -1,6 +1,72 @@
 const password = document.getElementById("password");
 const email = document.getElementById("email");
 const submit = document.getElementById("submit");
+var validP = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$/;
+var validE = /^([a-zA-Z0-9\.]+)@([a-zA-Z]+).([a-z]+).([a-z]+)?$/;
+
+users = {
+  emails: "",
+  passwords: "",
+};
 
 submit.addEventListener("click", store);
-function store() {}
+
+function store() {
+  if (password.value.match(validP) && email.value.match(validE)) {
+    users.emails += email.value;
+    users.passwords += password.value;
+  } else {
+    window.alert("Invalid");
+  }
+
+  console.log(users);
+}
+
+// let symbols = [
+//   "~",
+//   "!",
+//   "@",
+//   "#",
+//   "$",
+//   "%",
+//   "^",
+//   "&",
+//   "*",
+//   "(",
+//   ")",
+//   "_",
+//   "-",
+//   "+",
+//   "=",
+//   "{",
+//   "[",
+//   "}",
+//   "]",
+//   "|",
+//   '":',
+//   ";",
+//   "<",
+//   ">",
+//   ".",
+//   "?",
+//   "/",
+//   ",",
+// ];
+
+// function containsUppercase(str) {
+//   return /^[A-Z]+$/.test(str);
+// }
+// submit.addEventListener("click", store);
+// function store() {
+//   if (containsUppercase(password.value)) {
+//     for (let i = 0; i < symbols.length; i++) {
+//       if (password.value.includes(symbols[i])) {
+//         console.log(password.value);
+//       } else {
+//         console.log("invalid");
+//       }
+//     }
+//   } else {
+//     console.log("invalid");
+//   }
+// }
