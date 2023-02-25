1f8dd962c5adc80134586f826409ececf956710a
MohammadAlAgha <mohalagha11@gmail.com>
2023-02-25 18:40:25 +0200

    Added the prompt()

--- Js/merge.js
+++ Js/merge.js
@@ -1,1 +1,12 @@
-prompt("Enter a Number");
+let number = document.getElementById("number");
+
+let promtNum = prompt("Enter a Number");
+let toMerge = [];
+for (let i = 0; i < promtNum.length; i++) {
+  toMerge.push(promtNum[i]);
+}
+
+function merge(arr) {}
+
+merge(toMerge);
+console.log(toMerge);
