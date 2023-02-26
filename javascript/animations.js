f71c8311c810e467c2949e2037d0d0209420a9f4
MohammadAlAgha <mohalagha11@gmail.com>
2023-02-26 19:05:49 +0200

    Added some animations

--- javascript/animations.js
+++ javascript/animations.js
@@ -2,6 +2,7 @@ const ip = document.getElementById("ip");
 const home = document.getElementById("location");
 const sum = document.getElementById("sum");
 const end = document.getElementById("end");
+const rocket = document.getElementById("rocket");
 fetch("http://ip-api.com/json/?fields=61439")
   .then((response) => response.json())
   .then((data) => {
@@ -37,4 +38,16 @@ window.scroll = function () {
 end.addEventListener("click", () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+});
+
+rocket.addEventListener("click", function () {
+  let start = Date.now();
+
+  let timer = setInterval(function () {
+    let timePassed = Date.now() - start;
+
+    rocket.style.margin = timePassed / 10 + "px";
+
+    if (timePassed > 4000) clearInterval(timer);
+  }, 1);
 });
