ec2f20a31a97db064d6299558bcf6521239b1eaf
MohammadAlAgha <mohalagha11@gmail.com>
2023-02-26 10:21:17 +0200

    Optimized the palindrome

--- javascript/palindrome.js
+++ javascript/palindrome.js
@@ -5,21 +5,13 @@ done.addEventListener("click", function () {
   check(word.value);
 });
 function check(words) {
-  console.log(words);
   for (let i = 0; i < words.length; i++) {
     let j = words.length - 1 - i;
-    console.log(words[i]);
-    console.log(words[j]);
     if (words[i] == words[j]) {
       let portion = words.slice(i + 1, j - 1);
-      console.log(portion);
       check(portion);
       if ((portion.length = 1)) {
         result.innerHTML = "This is a Palindrome!!!";
-        console.log("Palindrome");
-      } else {
-        result.innerHTML = "Not Palindrome";
-        console.log("not Palindrome");
       }
     } else {
       result.innerHTML = "Not Palindrome";
